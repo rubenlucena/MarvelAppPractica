2ecc7b5aa17f6268080b1a53367800c3a15ba99b
Rubén Lucena <ruben.lucena@hey.com>
2021-09-26 15:09:23 +0200

    Initial Commit

---
+++ Shared/MarvelAppPracticaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelAppPracticaApp.swift
+//  Shared
+//
+//  Created by Ruben Lucena on 26/9/21.
+//
+
+import SwiftUI
+
+@main
+struct MarvelAppPracticaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
